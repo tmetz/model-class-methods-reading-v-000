47abe0599633f5433b5aa3754eb7b76feb4a3e5f
Tammy Metz <tammy@tammymetz.com>
2019-02-18 15:56:29 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,21 @@
 class PostsController < ApplicationController
 
-  helper_method :params
+  #helper_method :params
 
   def index
-    @posts = Post.all
+    @authors = Author.all
+
+    if !params[:author].blank?
+      @posts = Post.where(author: params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.where("created_at >= ?", Time.zone.today.beginning_of_day)
+      else
+        @posts = Post.where("created_at < ?"), Time.zone.today.beginning_of_day)
+      end
+    else
+      @posts = Post.all
+    
   end
 
   def show
